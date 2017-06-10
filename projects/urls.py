5fd07e6f106d7b9c1c37b5dbbce3950c15c5be79
Shirish Kadam <shirishkadam35@yahoo.com>
2017-06-10 22:27:29 +0530

    Added Projects detail view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import view_project
+from .views import view_project, project_detail
 
 urlpatterns = [
 
     url('^$', view_project),
+    url('^[0-9]', project_detail),
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def view_project(requests):
     return render(requests, 'project.html', {})
+
+
+def project_detail(requests):
+    return render(requests, 'detail_project.html', {})
