3a6a395c4ac5230b1c3d060cb89b3f704792afea
Shirish Kadam <shirishkadam35@yahoo.com>
2017-06-14 00:58:22 +0530

    Added Email API from MailGun

--- me/views.py
+++ me/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 # from django.core.mail import send_mail
 # Create your views here.
+from .emails import send_contact_message, send_simple_message
 
 
 def view_resume(request):
+
+    request_status = 0
+    con_name = "buddy"
+
     if request.method == "POST":
         con_name = request.POST.get("name", "")
         con_email = request.POST.get("email", "")
         con_subject = request.POST.get("subject", "")
         con_message = request.POST.get("message", "")
 
-        return render(request, 'resume.html', {})
-    return render(request, 'resume.html', {})
+        # mail_request = send_simple_message()      # Testing
+        mail_request = send_contact_message(con_name, con_email, con_subject, con_message)
+        request_status = mail_request.status_code
+        print(con_name, mail_request.status_code)
+
+        return render(request, 'resume.html', {'status_code': request_status, 'con_name': con_name})
+    return render(request, 'resume.html', {'status_code': request_status, 'con_name': con_name})
--- static/js/init.js
+++ static/js/init.js
@@ -4,6 +4,7 @@
         $('.button-collapse').sideNav();
         $('.slider').slider({interval:3000});
         $('.materialboxed').materialbox();
+        $('.modal').modal();
         $('.datepicker').pickadate({
             selectMonths: true, // Creates a dropdown to control month
             selectYears: 15, // Creates a dropdown of 15 years to control year
