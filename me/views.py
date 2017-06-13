34c1f4ea6ba5744304a2e3a418e50da3f23a41a6
Shirish Kadam <shirishkadam35@yahoo.com>
2017-06-13 23:50:44 +0530

    Fixed forms and POST requests

--- me/views.py
+++ me/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def view_resume(requests):
-    if requests.method == "POST":
-        pass
-    return render(requests, 'resume.html', {})
+def view_resume(request):
+    if request.method == "POST":
+        con_name = request.POST.get("name", "")
+        con_email = request.POST.get("email", "")
+        con_subject = request.POST.get("subject", "")
+        con_message = request.POST.get("message", "")
+
+        return render(request, 'resume.html', {})
+    return render(request, 'resume.html', {})
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import AddProject
 # Create your views here.
 
 
@@ -11,5 +11,21 @@ def project_detail(requests):
     return render(requests, 'detail_project.html', {})
 
 
-def add_project(requests):
-    return render(requests, 'add_project.html', {})
+def add_project(request):
+    if request.method == 'GET':
+        return render(request, 'add_project.html', {})
+
+    elif request.method == 'POST':
+
+        pro_title = request.POST.get("title", "")
+        pro_from = request.POST.get("date_from", "")
+        pro_to = request.POST.get("date_to", "")
+        pro_file = request.POST.get("file_name", "")
+        pro_link = request.POST.get("url_link", "")
+        pro_tech = request.POST.get("tech", "")
+        pro_descp = request.POST.get("description", "")
+
+        project = AddProject(project_title=pro_title, project_start=pro_from, project_end=pro_to, project_file=pro_file, project_url=pro_link, project_tech=pro_tech, project_description=pro_descp)
+        project.save()
+
+        return render(request, 'add_project.html', {})
