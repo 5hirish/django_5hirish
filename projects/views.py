77dab2f8b158f11193aba91396a4990203aba9ed
Shirish Kadam <shirishkadam35@yahoo.com>
2017-06-15 18:21:03 +0530

    View to add project

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import AddProject
+import time, datetime
 # Create your views here.
 
 
@@ -17,9 +18,16 @@ def add_project(request):
 
     elif request.method == 'POST':
 
+        date_from = datetime.datetime.strptime(request.POST.get("date_from", ""), "%d-%m-%Y").strftime('%Y-%m-%d')
+
+        if request.POST.get("date_to", "") == "":
+            date_to = datetime.date.today().strftime('%Y-%m-%d')
+        else:
+            date_to = datetime.datetime.strptime(request.POST.get("date_to", ""), "%d-%m-%Y").strftime('%Y-%m-%d')
+
         pro_title = request.POST.get("title", "")
-        pro_from = request.POST.get("date_from", "")
-        pro_to = request.POST.get("date_to", "")
+        pro_from = date_from
+        pro_to = date_to
         pro_file = request.POST.get("file_name", "")
         pro_link = request.POST.get("url_link", "")
         pro_tech = request.POST.get("tech", "")
